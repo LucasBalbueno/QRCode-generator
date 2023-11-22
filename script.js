4938cbd46d8d00e2b045cec2d46cec38dd294632
Lucas Balbueno Vicencio <182300633@senacinformatica.br>
2023-11-21 21:28:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const inputLink = document.getElementById('inputLink');
+const botaoGenerator = document.getElementById('botaoGenerator');
+const codeImg = document.getElementById('codeImg');
+const containerCode = document.getElementById('containerCode');
+
+botaoGenerator.addEventListener('click', () => {                               // MUDAR TIPO DO EVENTO
+    let conteudo = inputLink.value;
+
+    if (conteudo == '') {
+        alert('Insira um link válido');
+    } else {
+        botaoGenerator.innerText = 'Gerando QR Code! Aguarde...';
+
+        let qrcode = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${conteudo.trim()}`;
+
+        console.log(qrcode)
+        containerCode.classList.add('container__code-activated');
+
+        codeImg.classList.add('code__imagem-activated');
+        codeImg.src = qrcode;
+    }
+
+    codeImg.addEventListener('load', () => {
+        botaoGenerator.innerText = 'CONCLUIDO'
+        botaoGenerator.classList.add('generator__botao-concluded')
+    })
+})
